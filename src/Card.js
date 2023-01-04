899c218471ac657373584ffa4e6a2a80069c6298
Niklauzi <ftpfavour@gmail.com>
2023-01-04 22:32:48 +0100

    Robofriend starter component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src="https://robohash.org/titan?200x200" alt="bots" />
+        <div>
+            <h2>Jane Doe</h2>
+            <p> Janedoe@gmail.com</p>
+        </div>
+        </div>
+    )
+}
+
+export default Card
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Hello from './Hello';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello Hailing= {`Wagwan my titan`} />
+    <Card />
   </React.StrictMode>
 );
 
