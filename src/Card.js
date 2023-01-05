a7addff987f0011924fc1cfa75639af429bbf877
Niklauzi <ftpfavour@gmail.com>
2023-01-05 23:54:46 +0100

    Adding Card.js and Cardlist.js for the botfriends react-app

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, email, id}) => {
     return (
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img src={`https://robohash.org/${id}?200x200`} alt="bots" />
         <div>
-            <img src="https://robohash.org/titan?200x200" alt="bots" />
-        <div>
-            <h2>Jane Doe</h2>
-            <p> Janedoe@gmail.com</p>
+            <h2>{name}</h2>
+            <p> {email}</p>
         </div>
         </div>
     )
